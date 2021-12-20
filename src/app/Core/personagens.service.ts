f7dc048d3976b0799a9800144218423aa887c770
Daniel Mendes de Almeida <daniel.m.almeida.97@gmail.com>
2021-12-20 13:05:56 -0300

    service.ts
    criação da page service.ts

--- src/app/Core/personagens.service.ts
+++ src/app/Core/personagens.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BASE_URL } from './app.api';
+import { ListPersonagens} from '../cards/listPersonagens';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonagensService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getList(){
+    var urlString = `${BASE_URL}characters/${id}`
+    return this.http.get(urlString);
+  }
 }
---
+++ src/app/Pages/cards/listPersonagens.ts
@@ -0,0 +1,9 @@
+export interface ListPersonagens {
+    id:[null];
+    name: string;
+    img: [null];
+    birthday: string;
+    occupation: [null]; //seria um array
+    status: string;
+
+}
