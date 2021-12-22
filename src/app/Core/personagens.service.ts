4f6e87ca7d9f48036f44dbf425a1d573057105bc
Daniel Mendes de Almeida <daniel.m.almeida.97@gmail.com>
2021-12-22 20:08:10 -0300

    ListPersonagens
    Puxando dados do back para listar os personagens em cards

--- src/app/Core/personagens.service.ts
+++ src/app/Core/personagens.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BASE_URL } from './app.api';
+import { ListPersonagens } from '../Pages/cards/listPersonagens';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class PersonagensService {
 
   getList(){
     var urlString = `${BASE_URL}characters/`
-    return this.http.get(urlString);
+    return this.http.get<ListPersonagens[]>(urlString);
   }
 }
--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonagensService } from 'src/app/Core/personagens.service';
+import { ListPersonagens } from '../cards/listPersonagens';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  personagens!: ListPersonagens[];
+
+  users = [
+    { id: 1, name: 'João', apelido: 'Joãozinho', idade: 25},
+    { id: 2, name: 'Gabriela', apelido: 'Gabi', idade: 22},
+    { id: 3, name: 'Samira', apelido: 'Robin', idade: 21},
+    { id: 4, name: 'Carlos', apelido: 'Carlito', idade: 23},
+    { id: 5, name: 'Jhony', apelido: 'Bravo', idade: 30},
+  ];
+
+  constructor(private service: PersonagensService) { }
 
   ngOnInit(): void {
+    this.service.getList().subscribe(console.log);
   }
 
 }
