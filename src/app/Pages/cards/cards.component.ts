9a2ba71dad574570ef4fbc2d74d20b5fe06b80b0
Daniel Mendes de Almeida <daniel.m.almeida.97@gmail.com>
2021-12-22 16:31:49 -0300

    NgFor
    Incluindo um NgFor Fake para teste

--- src/app/Pages/cards/cards.component.ts
+++ src/app/Pages/cards/cards.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
+  users = [
+    { id: 1, name: 'João', apelido: 'Joãozinho', idade: 25},
+    { id: 2, name: 'Gabriela', apelido: 'Gabi', idade: 22},
+    { id: 3, name: 'Samira', apelido: 'Robin', idade: 21},
+    { id: 4, name: 'Carlos', apelido: 'Carlito', idade: 23},
+    { id: 5, name: 'Jhony', apelido: 'Bravo', idade: 30},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
